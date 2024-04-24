8e849c56d47ce455be2a8754c0d35540e174960d
crow <113790120+the1crows@users.noreply.github.com>
2024-04-24 16:21:57 +0000

    h

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -11,6 +11,8 @@ export function WelcomeBanner() {
       className="relative overflow-hidden flex flex-col items-center justify-center p-4 rounded-lg lg:grid lg:grid-cols-3 gap-4 lg:p-10 bg-no-repeat bg-cover"
       style={{
         backgroundImage: `url('/background.png')`, // Path to your background image
+        backgroundSize: "contain", // Adjust background size to contain
+        backgroundPosition: "center", // Center the background image
       }}
     >
       <div className="lg:col-span-2 text-center lg:text-left">
@@ -27,25 +29,25 @@ export function WelcomeBanner() {
       <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
         <button
           onClick={() => window.open("https://raydium.io/swap/")}
-          className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+          className="rounded-lg p-3 bg-purple-500 bg-opacity-50 hover:bg-purple-700 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           Buy $DOGE
         </button>
         <button
           onClick={openWhitepaper} // Use the function to open Whitepaper
-          className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+          className="rounded-lg p-3 bg-purple-500 bg-opacity-50 hover:bg-purple-700 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           Whitepaper
         </button>
         <button
           onClick={() => window.open("https://dexscreener.com")}
-          className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+          className="rounded-lg p-3 bg-purple-500 bg-opacity-50 hover:bg-purple-700 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           Chart
         </button>
         <button
           onClick={() => window.open("https://t.me/DogeBetsSol")}
-          className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+          className="rounded-lg p-3 bg-purple-500 bg-opacity-50 hover:bg-purple-700 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           💬 Join Telegram
         </button>
