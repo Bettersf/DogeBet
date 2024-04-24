9d6b7dfa2625120b1254cab4d51e0d019a751f86
crow <113790120+the1crows@users.noreply.github.com>
2024-04-24 16:04:29 +0000

    b

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -7,11 +7,16 @@ export function WelcomeBanner() {
   };
 
   return (
-    <div className="relative overflow-hidden flex flex-col items-center justify-center p-4 rounded-lg lg:grid lg:grid-cols-3 gap-4 lg:p-10 bg-gradient-to-br from-orange-300 via-pink-500 to-blue-500">
+    <div
+      className="relative overflow-hidden flex flex-col items-center justify-center p-4 rounded-lg lg:grid lg:grid-cols-3 gap-4 lg:p-10 bg-no-repeat bg-cover"
+      style={{
+        backgroundImage: `url('/background.png')`, // Path to your background image
+      }}
+    >
       <div className="lg:col-span-2 text-center lg:text-left">
         <div className="flex items-center justify-center md:justify-start">
           <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-          Welcome To DogeBets Casino!
+            Welcome To DogeBets Casino!
           </h1>
           <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
         </div>
