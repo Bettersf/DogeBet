a29cced825107898843b160f9212208f8992f3b3
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-05-07 14:34:38 +0200

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -10,13 +10,13 @@ export function WelcomeBanner() {
       <div className="lg:col-span-2 text-center lg:text-left">
         <div className="flex items-center justify-center md:justify-start">
           <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-            Welcome To DogeBets Casino!
+            Welcome To MONKI Casino!
           </h1>
           <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
         </div>
         <p className="mt-4 text-white drop-shadow">
-          Join our launch and take part in a Huge ecosystem! Soon we will open for staking and other utilities for the holders!
-           DogeBets CA: 6wX3vdFAaLKeUzoPjAi5uKcjyfNQSoosWaJi7NzSuFzf
+          Take part of a unique CTO, casino added as utility!
+           MONKI CA: HZKXhPGJaRUk7RDrLNjvdg871VKphcRAPeorP7HnBQAM
         </p>
       </div>
       <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
@@ -24,7 +24,7 @@ export function WelcomeBanner() {
           onClick={() => window.open("https://raydium.io/swap/?outputCurrency=6wX3vdFAaLKeUzoPjAi5uKcjyfNQSoosWaJi7NzSuFzf")}
           className="rounded-lg p-3 bg-[#6b46c1] bg-opacity-50 hover:bg-[#553c9a] hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
-          Buy $DOGE
+          Buy $MONKI
         </button>
         <button
           onClick={openWhitepaper} // Use the function to open Whitepaper
