4b945ec26589b697150a85b0730e29e65884d71c
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-05-13 12:29:10 +0200

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -10,36 +10,36 @@ export function WelcomeBanner() {
       <div className="lg:col-span-2 text-center lg:text-left">
         <div className="flex items-center justify-center md:justify-start">
           <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-            Welcome To MONKI Casino!
+            Welcome To $GEOG Woshigtun Casino
           </h1>
           <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
         </div>
         <p className="mt-4 text-white drop-shadow">
           Take part of a unique CTO, casino added as utility!
-          CA: HZKXhPGJaRUk7RDrLNjvdg871VKphcRAPeorP7HnBQAM
+          CA: FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9
         </p>
       </div>
       <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
         <button
-          onClick={() => window.open("https://raydium.io/swap/?outputCurrency=HZKXhPGJaRUk7RDrLNjvdg871VKphcRAPeorP7HnBQAM")}
+          onClick={() => window.open("https://raydium.io/swap/?outputCurrency=FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9")}
           className="rounded-lg p-3 bg-[#6b46c1] bg-opacity-50 hover:bg-[#553c9a] hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
-          Buy $MONKI
+          Buy $GEOG
         </button>
         <button
-          onClick={() => window.open("https://twitter.com/monkionsol")}
+          onClick={() => window.open("https://twitter.com/woshigtunCTO")}
           className="rounded-lg p-3 bg-[#6b46c1] bg-opacity-50 hover:bg-[#553c9a] hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           Twitter
         </button>
         <button
-          onClick={() => window.open("https://dexscreener.com/solana/e4xqrqd2ifnjphmtstsh4pavrzycygfmqqhbshxcufxj")}
+          onClick={() => window.open("https://dexscreener.com/solana/FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9?utm_source=fluxbot&id=1715539840")}
           className="rounded-lg p-3 bg-[#6b46c1] bg-opacity-50 hover:bg-[#553c9a] hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           Chart
         </button>
         <button
-          onClick={() => window.open("https://t.me/monkicto")}
+          onClick={() => window.open("https://t.me/GeogWoshigtunCTO")}
           className="rounded-lg p-3 bg-[#6b46c1] bg-opacity-50 hover:bg-[#553c9a] hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           💬 Join Telegram
