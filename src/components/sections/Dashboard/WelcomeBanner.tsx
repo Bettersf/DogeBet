789755f6cd872bfed6cdc56578ade7b61b010a9c
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-05-14 05:12:20 +0200

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -10,36 +10,36 @@ export function WelcomeBanner() {
   <div className="lg:col-span-2 text-center lg:text-left">
     <div className="flex items-center justify-center md:justify-start">
       <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-        Welcome To $GEOG Woshigtun Casino
+        Welcome To $GEOG Woshingtun presedential Casino
       </h1>
       <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
     </div>
-    <p className="mt-4 text-white drop-shadow">
+    <p className="mt-4 text-white font-bold drop-shadow">
       CA: FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9
     </p>
   </div>
   <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
     <button
       onClick={() => window.open("https://raydium.io/swap/?outputCurrency=FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9")}
-      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-black-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Buy $GEOG
     </button>
     <button
       onClick={() => window.open("https://twitter.com/woshigtunCTO")}
-      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-black-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Twitter
     </button>
     <button
       onClick={() => window.open("https://dexscreener.com/solana/FXwCaUML71j3BzCdwE79Aww3ghvPZPgcL74UY9gNhwP9?utm_source=fluxbot&id=1715539840")}
-      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-black-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Chart
     </button>
     <button
       onClick={() => window.open("https://t.me/GeogWoshigtunCTO")}
-      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-800 bg-opacity-75 hover:bg-black-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       💬 Join Telegram
     </button>
