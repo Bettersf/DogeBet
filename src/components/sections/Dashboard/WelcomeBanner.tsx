38ae62ecebb496d434c47199c326b6b039ee205a
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-07-11 03:19:51 +0200

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -10,35 +10,35 @@ export function WelcomeBanner() {
   <div className="lg:col-span-2 text-center lg:text-left">
     <div className="flex items-center justify-center md:justify-start">
       <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-        Welcome To $FLOKI Bets Casino. A utillity for Floki on solana CTO!
+        Welcome To $SHIB Casino
       </h1>
-      <img src="/wave.gif" className="h-12" alt="Gamba Logo" />
+      <img src="" className="h-12" alt="Gamba Logo" />
     </div>
     <p className="mt-4 text-white font-bold drop-shadow">
-      CA: NiA4syconyg4fsiKQeLEPWBBB3ykXoCccGqxMT7iX74
+      CA: AyX9ETMfYzM7MLBE6DVDsJzmAQokcQUcJqKxtSMs5HKh
     </p>
   </div>
   <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
     <button
-      onClick={() => window.open("https://raydium.io/swap/?outputMint=NiA4syconyg4fsiKQeLEPWBBB3ykXoCccGqxMT7iX74&inputMint=sol")}
+      onClick={() => window.open("https://raydium.io/swap/?outputMint=AyX9ETMfYzM7MLBE6DVDsJzmAQokcQUcJqKxtSMs5HKh&inputMint=sol")}
       className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-black-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Buy $FLOKI
     </button>
     <button
-      onClick={() => window.open("https://twitter.com/FlokiOnSolCTO")}
+      onClick={() => window.open("https://x.com/shibainulfg")}
       className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Twitter
     </button>
     <button
-      onClick={() => window.open("https://dexscreener.com/solana/J8JBUEyH5zKQv6Z39aGLYLfVfUR5CxcyzwkgadqJbrsx")}
+      onClick={() => window.open("https://dexscreener.com/solana/9xyfmkfih3hjhaacxhpkhvzc99nzdpstkofuytbofxkt")}
       className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Chart
     </button>
     <button
-      onClick={() => window.open("https://t.me/FlokiSolCt0")}
+      onClick={() => window.open("https://t.me/shibainulfg")}
       className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       💬 Join Telegram
