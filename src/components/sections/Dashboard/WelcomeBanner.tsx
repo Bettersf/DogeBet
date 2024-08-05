a793c3a4b49a057e5a3fff5eab028e6aec6d7722
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-08-05 23:35:51 +0200

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -21,25 +21,25 @@ export function WelcomeBanner() {
   <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
     <button
       onClick={() => window.open("https://raydium.io/swap/?outputMint=AyX9ETMfYzM7MLBE6DVDsJzmAQokcQUcJqKxtSMs5HKh&inputMint=sol")}
-      className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-900 bg-opacity-75 hover:bg-black-800 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Buy $SHIB
     </button>
     <button
       onClick={() => window.open("https://x.com/shibainulfg")}
-      className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-900 bg-opacity-75 hover:bg-black-800 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Twitter
     </button>
     <button
       onClick={() => window.open("https://dexscreener.com/solana/9xyfmkfih3hjhaacxhpkhvzc99nzdpstkofuytbofxkt")}
-      className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-900 bg-opacity-75 hover:bg-black-800 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       Chart
     </button>
     <button
       onClick={() => window.open("https://t.me/shibainulfg")}
-      className="rounded-lg p-3 bg-black-800 bg-opacity-75 hover:bg-gray-900 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
+      className="rounded-lg p-3 bg-gray-900 bg-opacity-75 hover:bg-black-800 hover:-translate-y-0.5 transform text-white font-bold transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
     >
       💬 Join Telegram
     </button>
