f1500e4b8011de75244e4f6b31cbfabc478d154f
Bettersf <166757622+Bettersf@users.noreply.github.com>
2024-08-05 22:58:27 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -17,7 +17,7 @@ export const PLATFORM_CREATOR_FEE = 0.05; // 5% (5/100 = 0.05)
 export const PLATFORM_JACKPOT_FEE = 0.01; // 1% (1/100 = 0.01)
 
 // Platform URL - Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = "floki-bets.online";
+export const PLATFORM_SHARABLE_URL = "https://dogebets.casino";
 
 // Toggle all live events acrossed gamba toast (true = on, false = off)
 export const LIVE_EVENT_TOAST = true;
